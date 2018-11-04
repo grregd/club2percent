5d988c5f9f1d208797a517e24b5ed8a5c0f4a7f3
grregd <grregd@gmail.com>
2018-11-04 01:31:52 +0100

    update

--- level_design/quizdb.js
+++ level_design/quizdb.js
@@ -16,21 +16,25 @@ function makeQuizItem( aNum, aHint, aAnswer ) {
 }
 
 var quizData = [
-makeQuizItem( 78, "", ["c", "cz", "g", "b", "y", "f", "p", "r", "w", "z"] ),
-makeQuizItem( 61, "", ["g", "c", "y", "b", "cz", "r", "f", "p", "z", "w"] ),
-makeQuizItem( 82, "", ["c", "b", "g", "cz", "y", "f", "p", "w", "r", "z"] ),
-makeQuizItem( 55, "", ["cz", "c", "b", "y", "g", "r", "p", "w", "z", "f"] ),
-makeQuizItem( 52, "", ["b", "c", "g", "cz", "y", "p", "w", "r", "f", "z"] ),
-makeQuizItem( 62, "", ["g", "y", "c", "b", "cz", "f", "z", "r", "p", "w"] ),
-makeQuizItem( 69, "", ["g", "cz", "c", "b", "y", "f", "z", "w", "r", "p"] ),
-makeQuizItem( 79, "", ["c", "b", "cz", "y", "g", "z", "r", "p", "f", "w"] ),
-makeQuizItem( 65, "", ["cz", "g", "y", "c", "b", "f", "p", "w", "r", "z"] ),
-makeQuizItem( 60, "", ["g", "c", "cz", "b", "y", "w", "f", "r", "p", "z"] ),
-makeQuizItem( 75, "", ["c", "b", "cz", "y", "g", "w", "p", "f", "z", "r"] ),
-makeQuizItem( 59, "", ["g", "c", "cz", "b", "y", "z", "w", "p", "f", "r"] ),
-makeQuizItem( 74, "", ["y", "g", "c", "b", "cz", "z", "p", "f", "w", "r"] ),
+makeQuizItem( 50, "", ["c", "g", "b", "y", "cz", "w", "p", "z", "r", "f"] ),
 makeQuizItem( 51, "", ["cz", "g", "c", "y", "b", "w", "z", "r", "f", "p"] ),
+makeQuizItem( 52, "", ["b", "c", "g", "cz", "y", "p", "w", "r", "f", "z"] ),
+makeQuizItem( 55, "", ["cz", "c", "b", "y", "g", "r", "p", "w", "z", "f"] ),
+makeQuizItem( 59, "", ["g", "c", "cz", "b", "y", "z", "w", "p", "f", "r"] ),
+makeQuizItem( 60, "", ["g", "c", "cz", "b", "y", "w", "f", "r", "p", "z"] ),
+makeQuizItem( 61, "", ["g", "c", "y", "b", "cz", "r", "f", "p", "z", "w"] ),
+makeQuizItem( 62, "", ["g", "y", "c", "b", "cz", "f", "z", "r", "p", "w"] ),
+makeQuizItem( 64, "", ["cz", "c", "g", "y", "b", "z", "f", "p", "r", "w"] ),
+makeQuizItem( 65, "", ["cz", "g", "y", "c", "b", "f", "p", "w", "r", "z"] ),
+makeQuizItem( 69, "", ["g", "cz", "c", "b", "y", "f", "z", "w", "r", "p"] ),
+makeQuizItem( 72, "", ["b", "g", "y", "cz", "c", "r", "p", "z", "f", "w"] ),
+makeQuizItem( 74, "", ["y", "g", "c", "b", "cz", "z", "p", "f", "w", "r"] ),
+makeQuizItem( 75, "", ["c", "b", "cz", "y", "g", "w", "p", "f", "z", "r"] ),
+makeQuizItem( 78, "", ["c", "cz", "g", "b", "y", "f", "p", "r", "w", "z"] ),
+makeQuizItem( 79, "", ["c", "b", "cz", "y", "g", "z", "r", "p", "f", "w"] ),
+makeQuizItem( 81, "", ["g", "b", "cz", "c", "y", "p", "f", "r", "z", "w"] ),
+makeQuizItem( 82, "", ["c", "b", "g", "cz", "y", "f", "p", "w", "r", "z"] ),
 makeQuizItem( 83, "", ["cz", "y", "g", "c", "b", "f", "z", "r", "w", "p"] ),
-makeQuizItem( 72, "", ["b", "g", "y", "cz", "c", "r", "p", "z", "f", "w"] )
+makeQuizItem( 84, "", ["y", "c", "g", "cz", "b", "z", "w", "r", "p", "f"])
 ];
 
